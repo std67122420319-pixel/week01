c210daa3dfac154adca89a0ae7a443a6cda66f0b
std67122420319-pixel <std.67122420319@ubru.ac.th>
2025-11-28 11:38:43 +0700

    edit

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 def index():
-  return '<h1>Hello World!</h1>'
+  return '<h1>deeee</h1>'
 
 if __name__ == '__main__':
   app.run(debug=True, port=3000)
